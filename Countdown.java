2509621e81382e338b9a59ba95e1241edb6e3170
MustapakulovAzamat <azamustapakylov6@gmail.com>
2026-09-21 22:52:06 +0600

    ex-7

---
+++ Countdown.java
@@ -0,0 +1,18 @@
+public class Countdown {
+  public static void main(String[] args) {
+    int i = 10;
+    while (i >= 1) {
+      System.out.println(i);
+      i--;
+    }
+    System.out.println("Lift off!");
+
+    System.out.println(); 
+
+    for (int k = 10; k >= 1; k--) {
+      System.out.println(k);
+    }
+    System.out.println("Lift off!");
+  }
+}
+ 
